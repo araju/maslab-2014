9f33186c8a7c743b34f9c388ded502fa934d3808
Erick Fuentes <fuentes.erick@gmail.com>
2014-01-07 14:28:40 -0500

    Python Script to communicate with maple
    The python script will ramp from zero to full speed forward and then
    full speed back.

--- maple/serialtest.py
+++ maple/serialtest.py
@@ -1,20 +1,33 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyACM0', 115200)
+ser = serial.Serial('/dev/tty.usbmodem1411', 115200)
 
 def read():
-    print ser.read()
+    # print ser.read()
+    pass
 
+i = 0
+dir = 1
 while True:
     ser.write('S')
     read()
-    ser.write('\x00')
+    ser.write(chr(i & 0xFF))
     read()
-    ser.write('\x00')
+    ser.write(chr(i & 0xFF))
     read()
     ser.write('E')
     read()
     
-    time.sleep(1)
+    i += dir
+    if i == 127:
+        dir = -1
+
+    if i == -127:
+        dir = +1
+
+    print i
     
+
+    time.sleep(.1)
+
