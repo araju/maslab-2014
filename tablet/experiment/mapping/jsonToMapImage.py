c56e16466c334d16cf01574a39264901b1b638ed
Erick Fuentes <fuentes.erick@gmail.com>
2014-01-11 12:08:23 -0500

    jsonToMapImage works correctly

--- tablet/experiment/mapping/jsonToMapImage.py
+++ tablet/experiment/mapping/jsonToMapImage.py
@@ -1,16 +1,39 @@
 import json
 import sys
+from PIL import Image, ImageDraw
 
+#Assume that the map coordinates are in meters
+#Assume that each pixel is 1cm x 1cm
+
+SCALE_FACTOR = 100
 
 def main(fName):
     mapPoints = ''
     with open(fName) as fileIn:
         mapPoints = json.load(fileIn)['map']
-    print mapPoints
-    for point in mapPoints:
-        point[1] += 1
 
-    print mapPoints
+    maxX = 0
+    maxY = 0
+
+    for i in range(len(mapPoints)):
+
+        mapPoints[i] = (mapPoints[i][0] * SCALE_FACTOR, \
+                        mapPoints[i][1] * SCALE_FACTOR + 1)
+        
+        if mapPoints[i][0] > maxX:
+            maxX = mapPoints[i][0]
+
+        if mapPoints[i][1] > maxY:
+            maxY = mapPoints[i][1]
+
+    im = Image.new('RGB', (maxX, maxY), "white")
+    imDraw = ImageDraw.Draw(im)
+
+    for i in range(len(mapPoints)-1):
+        imDraw.line([mapPoints[i], mapPoints[i+1]], fill = 'black')
+
+
+    im.show()
 
 if __name__ == '__main__':
     fName = 'map.json'
