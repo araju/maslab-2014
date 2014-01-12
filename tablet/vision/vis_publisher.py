f9972cc94acd07e67db26b817e106980f137a736
U-akhil-HP\akhil <akhil@akhil-HP.(none)>
2014-01-12 15:17:00 -0500

    change the tabs to spaces, i think. and add vis_pub and process_blobs

---
+++ tablet/vision/vis_publisher.py
@@ -0,0 +1,4 @@
+# vis_publisher.py
+# Publishes the vision info to some socket
+# Uses the same json protocol as BotClient:
+# {"token":<yourtokenhere>, <field_identifier>:[<somestring>,<somestring>]}done\n
