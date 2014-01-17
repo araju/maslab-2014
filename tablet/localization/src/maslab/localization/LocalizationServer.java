d4f7a7b2cd53f9314339f88f88342d2b0814c7d5
araju <akhilraju@gmail.com>
2014-01-17 01:48:52 -0500

    adding mock map and avg point to localization

--- tablet/localization/src/maslab/localization/LocalizationServer.java
+++ tablet/localization/src/maslab/localization/LocalizationServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,6 +46,7 @@ public class LocalizationServer {
 	 * input = JSON. format: {"measurements":["<number>","<number>",...]}
 	 * output = json. format: {"confidentPoints" : [["<x>","<y>","theta"],...]}
 	 */
+	@SuppressWarnings("unchecked")
 	public void startServer() {
 		try {
 			this.cliSock = svrSock.accept();
@@ -80,8 +82,15 @@ public class LocalizationServer {
 				//TODO: handle too many points to return
 				
 				//write most confident points back to client in json
-				Map<String,List<List<Float>>> map = new HashMap<String,List<List<Float>>>();
+//				Map<String,List<List<Float>>> map = new HashMap<String,List<List<Float>>>();
+				Map map = new HashMap();
 				map.put("confidentPts", points);
+				List<Float> avg = findAvgPoint(points);
+				map.put("point", avg);
+				for (float f: avg) {
+					System.out.print(f + " , ");
+				}
+				System.out.println("");
 				output = JSONValue.toJSONString(map);
 //				System.out.println(output);
 				outputWriter.println(output);
@@ -97,6 +106,23 @@ public class LocalizationServer {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	
+	public static List<Float> findAvgPoint(List<List<Float>> pts) {
+		List<Float> avg = new ArrayList<Float>();
+		float avgX = 0;
+		float avgY = 0;
+		float avgTheta = 0;
+		for (List<Float> pt : pts) {
+			avgX += pt.get(0);
+			avgY += pt.get(1);
+			avgTheta += pt.get(2);
+		}
+		avg.add(avgX / pts.size());
+		avg.add(avgY / pts.size());
+		avg.add(avgTheta / pts.size());
+		return avg;
 	}
 	
 	
