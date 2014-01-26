e6eafb759515ae5f8f5bea072f0e7664df0cd21d
araju <akhilraju@gmail.com>
2014-01-26 18:33:42 -0500

    make crazy bot use the sensor manager

--- tablet/crazy_bot.py
+++ tablet/crazy_bot.py
@@ -12,32 +12,34 @@ import traceback
 import math
 from music import playMusic
 import random
+from sensor_manager import SensorManager
 
 class CrazyBot:
     MOVE_FORWARD, BACK_UP, SEARCH_DIRECTION, TURN_TO_DIR = ("moveForward", "backup", "search", "turnToDir")
 
-    def __init__(self):
-        self.state = self.MOVE_FORWARD
+    def __init__(self, maple, manager):
+        self.state = self.SEARCH_DIRECTION
         # self.state = self.BACK_UP
         self.stateStartTime = time.time();
-        self.maple = Maple()
+        self.maple = maple
+        self.sensorManager = manager
         self.driver = MotorDriver(self.maple)
-        self.odo = Odometry(self.maple)
-        self.bumps = BumpSensors(4,self.maple)
-        self.sonars = DistanceSensors(4,self.maple)
+        # self.odo = Odometry(self.maple)
+        # self.bumps = BumpSensors(4,self.maple)
+        # self.sonars = DistanceSensors(4,self.maple)
         
-        self.mapleRead = True
-        self.t = Thread(target = self.readMaple)
-        self.t.start()
+        # self.mapleRead = True
+        # self.t = Thread(target = self.readMaple)
+        # self.t.start()
         self.distances = [-1 for i in range(36)]
         self.halfFlag = 0
-        playMusic.play()
+        # playMusic.play()
 
 
-    def readMaple(self):
-        while self.mapleRead:
-            self.maple.periodic_task()
-            time.sleep(0.01)
+    # def readMaple(self):
+    #     while self.mapleRead:
+    #         self.maple.periodic_task()
+    #         time.sleep(0.01)
 
     def moveForwardSetup(self):
         self.stateStartTime = time.time()
@@ -48,9 +50,9 @@ class CrazyBot:
         # print 'move forward bitches'
         self.driver.driveMotors(15)
 
-        print self.sonars.distances[0]
+        # print "Sonar Distances: ", self.sensorManager.sonars.distances[0]
 
-        if (self.sonars.distances[0] < 20):
+        if (self.sensorManager.sonars.distances[0] < 20):
             print 'staph dood'
             self.driver.driveMotors(0)
             return self.backUpSetup()
@@ -59,16 +61,15 @@ class CrazyBot:
 
     def backUpSetup(self):
         self.stateStartTime = time.time()
-        self.odo.distance = 0
+        self.sensorManager.odo.distance = 0
         self.driver.driveMotors(-35)
         return self.BACK_UP
 
     def backUp(self):
 
-        print self.odo.distance
+        # print "Odo dist: ", self.sensorManager.odo.distance
 
-        if self.odo.distance < -30:
-
+        if self.sensorManager.odo.distance < -30:
             self.driver.driveMotors(0)
             return self.searchDirectionSetup()
 
@@ -78,23 +79,22 @@ class CrazyBot:
         self.stateStartTime = time.time()
         self.halfFlag = 0
         self.distances = [-1 for i in range(36)]
-        # return self.SEARCH_DIRECTION
-        return self.turnToDirSetup()
+        return self.SEARCH_DIRECTION
 
     def searchDirection(self):
-        if (time.time() - self.stateStartTime < .125 and self.halfFlag == 0) or (self.halfFlag == 1 and self.odo.direction > 175):
+        if (time.time() - self.stateStartTime < .125 and self.halfFlag == 0) or (self.halfFlag == 1 and self.sensorManager.odo.direction > 175):
             self.halfFlag += 1
             print 'Start Turn'
             self.driver.turnMotors(180)
-            self.odo.direction = 0
+            self.sensorManager.odo.direction = 0
             
-        print self.sonars.distances
+        # print self.sensorManager.sonars.distances
 
-        idx = int(math.floor(self.odo.direction/10) + (self.halfFlag - 1) * 18) % 36
-        if (self.distances[idx] < self.sonars.distances[0]):
-            self.distances[idx] = self.sonars.distances[0]
+        idx = int(math.floor(self.sensorManager.odo.direction/10) + (self.halfFlag - 1) * 18) % 36
+        if (self.distances[idx] < self.sensorManager.sonars.distances[0]):
+            self.distances[idx] = self.sensorManager.sonars.distances[0]
         
-        if self.odo.direction > 175 and self.halfFlag == 2:
+        if self.sensorManager.odo.direction > 175 and self.halfFlag == 2:
             print 'End Turn'
             self.stateStartTime = time.time()
             self.halfFlag = 0;
@@ -104,16 +104,17 @@ class CrazyBot:
     def turnToDirSetup(self):
         self.stateStartTime = time.time()
 
+        print "Distances: ", self.distances
         maxVal = max(self.distances)
         maxIdx = self.distances.index(maxVal)
         self.maxDir = maxIdx * 10
 
-        self.maxDir = random.randint(0, 359)
-        print self.maxDir
+        # self.maxDir = random.randint(0, 359)
+        # print self.maxDir
 
         self.driver.turnMotors(self.maxDir)
 
-        self.odo.direction = 0
+        self.sensorManager.odo.direction = 0
 
         print 'Max distance Angle:', self.maxDir
         return self.TURN_TO_DIR
@@ -121,42 +122,45 @@ class CrazyBot:
     # Assumes that self.distances is populated with a list of distances
     # The index will the heading / 10
     def turnToDir(self):
-
-        # print self.maxDir - 5, self.odo.direction, (self.maxDir + 5) % 360
-
-        if self.odo.direction > (self.maxDir - 5) and \
-            self.odo.direction < (self.maxDir + 5) % 360:
+        if self.sensorManager.odo.direction > (self.maxDir - 5) and \
+            self.sensorManager.odo.direction < (self.maxDir + 5) % 360:
             print 'Done Bitches'
             return self.moveForwardSetup()
-            # Change state
-            pass
         return self.TURN_TO_DIR
+
+    def reset(self):
+        self.state = self.searchDirectionSetup()
 
     def mainLoop(self):
         while True:
-            
-            if self.state == self.MOVE_FORWARD:
-                self.state = self.moveForward()
-                if time.time() - self.stateStartTime > 15:
-                    self.state = self.backUpSetup()
-            elif self.state == self.BACK_UP:
-                self.state = self.backUp()
-                if time.time() - self.stateStartTime > 15:
-                    self.state = self.searchDirectionSetup()
-            elif self.state == self.SEARCH_DIRECTION:
-                self.state = self.searchDirection()
-                if time.time() - self.stateStartTime > 15:
-                    self.state = self.turnToDirSetup()
-            elif self.state == self.TURN_TO_DIR:
-                self.state = self.turnToDir()
-                if time.time() - self.stateStartTime > 15:
-                    self.state = self.moveForwardSetup()
+            self.mainIter()
             time.sleep(.05)
+
+    def mainIter(self):
+        # print "State: ", self.state
+        if self.state == self.MOVE_FORWARD:
+            self.state = self.moveForward()
+            if time.time() - self.stateStartTime > 15:
+                self.state = self.backUpSetup()
+        elif self.state == self.BACK_UP:
+            self.state = self.backUp()
+            if time.time() - self.stateStartTime > 15:
+                self.state = self.searchDirectionSetup()
+        elif self.state == self.SEARCH_DIRECTION:
+            self.state = self.searchDirection()
+            if time.time() - self.stateStartTime > 15:
+                self.state = self.turnToDirSetup()
+        elif self.state == self.TURN_TO_DIR:
+            self.state = self.turnToDir()
+            if time.time() - self.stateStartTime > 15:
+                self.state = self.moveForwardSetup()
 
 
 
 if __name__ == '__main__':
-    c = CrazyBot()
+    m = Maple()
+    sense = SensorManager(2300, m)
+    c = CrazyBot(m, sense)
     try:
         c.mainLoop()
     except:
